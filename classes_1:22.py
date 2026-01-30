eafbd26fd1f087e5353aab79ae906374a07fb371
macluber29-collab <mac.luber29@paceacademy.org>
2026-01-30 10:07:48 -0500

    Update classes_1:22.py

--- classes_1:22.py
+++ classes_1:22.py
@@ -3,10 +3,32 @@ import time
 
 weaponlist = ["Kicking Boots", "Dagger", "Longsword"]
 potlist = ["Potion of Healing", "Potion of Swiftness", "Potion of Strength", "Potion of Luck"]
-backpack = []
 
 
 #class 1
+class Inventory:
+    def __init__(self,items=None):
+        if items is None:
+            items = []
+            self.items = items
+    
+    def add_item(self,item):
+        self.items.append(item)
+        print(f"added {item.name} to inventory.")
+    
+    def remove_item(self, item):
+        if item in self.items:
+            self.items.remove(item)
+            print(f"Remove {item.name} from inventory.")
+        else:
+            print(f"{item.name} not found in inventory.")
+    def display_inventory(self):
+        if self.items:
+            print("Inventory: ")
+            for item in self.items:
+                print(f"- {item.name}")
+        else:
+            print(f"Inventory is empty.")
 class Entity:
     def __init__(self, health, level, xp, size, race, strength):
         self.health = health
@@ -15,6 +37,7 @@ class Entity:
         self.size = size
         self.race = race
         self.strength = strength
+        self.inventory = Inventory()
    
     def xpgain(self, mult):
         gain = random.randint(1,3)
@@ -25,7 +48,8 @@ class Entity:
             self.level += 1
             print(f"You leveled up to level {self.level}")
             requirement = self.level*2
-        print(f"You now have {self.xp} xp.")
+            round(self.xp)
+        print(f"You now have {self.xp} xp out of {requirement} xp required for level {self.level + 1}")
 
     def take_damage(self, amount):
         self.health -= amount
@@ -43,11 +67,18 @@ class Entity:
 
         target.take_damage(total_damage)
 
-        weapon.degrade()
+        weapon.degrade(self.inventory)
 
         if target.health <= 0:
             print(f"You killed {target.race}!")
             self.xpgain(2)
+    def add_weapon_to_inventory(self,weapon):
+        self.inventory.add_item(weapon)
+        print(f"{self.race} equips {weapon.name}.")
+
+    def remove_weapon_from_inventory(self,weapon):
+        self.inventory.remove_item(weapon)
+        print(f"{self.race} unequips {weapon.name}.")
 
     def encounter(self, level):
         roll = random.randint(1,100)
@@ -68,12 +99,12 @@ class Weapon:
         self.special = special
 
 
-    def degrade(self):
-        if self.durability > 0:
-            self.durability -= 1
-            print(f"{self.name} durability: {self.durability}")
-            if self.durability <= 0:
-                print(f"{self.name} broke!")
+    def degrade(self, inventory):
+        self.durability -= 1
+        print(f"{self.name} durability: {self.durability}")
+        if self.durability <= 0:
+            print(f"{self.name} broke!")
+            inventory.remove(self)
 
 
 
@@ -86,10 +117,8 @@ class main():
     hero = Entity(20, 1, 0, "large", "Hero", 1)
     enemy = Entity(15, 1, 0, "tiny", "Goat", 1)
     kicking_boots = Weapon("Kicking Boots", 15, 10, "10000", "mythical", "roundhouse")
-    backpack.append(kicking_boots)
-    for i in range(len(backpack)):
-        print(backpack[0].name)
     hooves = Weapon("Hooves", 1, 15, "tiny", "common", "ram")
+    hero.add_weapon_to_inventory(kicking_boots)
 
     print(f"Behold your mighty hero!!! {hero.describe()}")
     print(f"{enemy.describe()}")
